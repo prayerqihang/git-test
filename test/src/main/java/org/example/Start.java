a7cb772fedd13c9f1e2a8ebae41bb23a444e5a4c
prayer <2303814422@qq.com>
2024-09-28 20:30:04 +0800

    hot-fix commit1

--- test/src/main/java/org/example/Start.java
+++ test/src/main/java/org/example/Start.java
@@ -4,5 +4,6 @@ public class Start {
     public static void main(String[] args) {
         System.out.println("Hello Maven");
         System.out.println("AAA");
+        System.out.println("hot-fix");
     }
 }
